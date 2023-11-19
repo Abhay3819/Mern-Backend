bdff7610382dca6d52ea3ef453f75e0b5693a34e
Abhay3819 <abhaysharma3819@gmail.com>
2023-11-20 00:39:51 +0530

    first commit

---
+++ db/conn.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://abhaysharma3819:abhay790538@cluster0.fyrcg5y.mongodb.net/you?retryWrites=true&w=majority";
+    // "your-mongodb-url"; pasword me @ ki jgh pr ye %40 likhega some special case : / ? # [ ] @
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+// export default connectToDB;
+connectToDB();
