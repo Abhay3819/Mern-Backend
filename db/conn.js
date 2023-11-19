46f0740996e4ca192c85fd1278abe33e09522424
Abhay3819 <abhaysharma3819@gmail.com>
2023-11-20 01:02:14 +0530

    changes

--- db/conn.js
+++ db/conn.js
@@ -1,23 +1,38 @@
 const mongoose = require("mongoose");
 
 
-const configOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
 
-const connectToDB = async () => {
-  const connectionUrl =
-    "mongodb+srv://abhaysharma3819:abhay790538@cluster0.fyrcg5y.mongodb.net/you?retryWrites=true&w=majority";
-    // "your-mongodb-url"; pasword me @ ki jgh pr ye %40 likhega some special case : / ? # [ ] @
+const DB = process.env.DATABASE
 
-  mongoose
-    .connect(connectionUrl, configOptions)
-    .then(() => console.log("Database connected successfully!"))
-    .catch((err) =>
-      console.log(`Getting Error from DB connection ${err.message}`)
-    );
-};
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DataBase Connected"))
+  .catch((err) => {
+    console.log(err);
+  })
 
-// export default connectToDB;
-connectToDB();
+
+       
+// const configOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+
+// const connectToDB = async () => {
+//   const connectionUrl =
+//     "mongodb+srv://abhaysharma3819:abhay790538@cluster0.fyrcg5y.mongodb.net/you?retryWrites=true&w=majority";
+//     // "your-mongodb-url"; pasword me @ ki jgh pr ye %40 likhega some special case : / ? # [ ] @
+
+//   mongoose
+//     .connect(connectionUrl, configOptions)
+//     .then(() => console.log("Database connected successfully!"))
+//     .catch((err) =>
+//       console.log(`Getting Error from DB connection ${err.message}`)
+//     );
+// };
+
+// // export default connectToDB;
+// connectToDB();
